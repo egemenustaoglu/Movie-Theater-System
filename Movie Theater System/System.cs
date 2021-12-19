7e0008791a790ad7e8c78e956149fc04327667d0
AtakanAkyaz <61373558+AtakanAkyaz@users.noreply.github.com>
2021-12-19 19:25:04 +0300

    19.12.2021 19:24
    usage of polymorphism

---
+++ Movie Theater System/System.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Movie_Theater_System
+{
+    class System
+    {
+        static void Main(string[] args)
+        {
+            
+            User user1 = new Manager("Atakan", 1);
+            User user2 = new TicketOfficer("Egoş", 2);
+            Asd(user1);
+            Asd(user2);
+        }
+
+        //Here we create a method for show polymorphism 
+        public static void Asd(User user)
+        {
+            user.userWorking();
+        }
+    }
+}

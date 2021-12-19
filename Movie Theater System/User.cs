1bebdba2be55fd765943497d2d7d864024659e4e
egemenustaoglu <hello@egemenustaoglu.com>
2021-12-19 19:34:17 +0300

    v1.0
    v1.0

---
+++ Movie Theater System/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie_Theater_System
+{
+    public abstract class User
+    {
+
+        public User()
+        {
+        }
+
+        //workerTalks() method for further usage of  Polymorphism
+        public abstract void userWorking();
+    }
+}
